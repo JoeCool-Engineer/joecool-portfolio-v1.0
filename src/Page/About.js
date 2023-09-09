dafb2b2e2a21f83b67ec428432ee08e0bc585d73
JoeCool-Engineer <rodney.buller2+github@gmail.com>
2023-09-09 09:03:40 -0700

    Updated About me page

--- src/Page/About.js
+++ src/Page/About.js
@@ -5,11 +5,19 @@ const About = () => {
     <>
     <div className='d-flex align-items-center'>
       <i className='fas fa-user-circle me-4'></i>
-      <p>Ex ea magna dolor consectetur deserunt cupidatat eu. Eiusmod anim aliqua cupidatat esse id eiusmod magna. Qui labore dolor eu sunt do do ipsum commodo laborum velit. Occaecat velit ad nisi occaecat. Eu incididunt anim labore mollit culpa id. Non incididunt esse officia occaecat enim amet qui magna fugiat do. Laborum ad do Lorem culpa minim anim reprehenderit commodo ipsum pariatur consequat dolor enim. Exercitation minim velit do esse.</p>
+      <p>I am a Mechanical Engineer with a healthy appetite to learn and be the very best that I can be. Eager to learn the most so that I may become a competitive individual within my field. With this being said, I’m a firm believer in the idea that people become stronger in their fields as they teach or pass on the knowledge/skills that they have obtained. I work very well in teams but at the same time a self-starter and self-reliant individual capable of greatness.</p>
     </div>
-      <p>Dolor exercitation culpa aliquip ut. Excepteur enim cupidatat exercitation duis nostrud proident culpa exercitation fugiat Lorem pariatur elit in. Cupidatat amet qui culpa laboris. Consequat dolor do anim deserunt eiusmod nisi mollit consequat ut sit sit amet. Cillum voluptate consectetur commodo incididunt. Amet cillum velit anim dolore esse tempor eiusmod consectetur deserunt eu. Eu fugiat in ex sint dolor fugiat.</p>
+      <p>My greatest and most rewarding achievement was the making the transition from military to school and then back to the work force which, has shown my ability to adapt and overcome to any obstacles.</p>
 
-      <p>Labore est sit officia sit occaecat ut dolore mollit occaecat. Laborum id laboris nulla esse velit in pariatur. Veniam deserunt dolore ullamco do. Laboris est ipsum exercitation officia ullamco magna sunt do ea est. Incididunt Lorem ut proident deserunt sint nulla nostrud consequat est excepteur dolor et labore. Excepteur ipsum ipsum ad et aliqua in est aliquip officia deserunt. Eiusmod duis quis cupidatat tempor tempor ad Lorem minim occaecat. Nulla et minim eu aute irure ad id excepteur. Esse et aute nostrud laboris sunt consequat non ex velit laboris et mollit eu.</p>
+      <p>I thrive off of positions where I express values such as creativity and ingenuity. Most importantly, when I’m able to see my achievements this renews my drive to make further impacts. </p>
+
+      <p>I have a great ability to pick up new trades specifically those that require a great deal of hands on activity to include automotive, HVAC repair, and machine shop manufacturing. It gives me a great since of pride to possess skills that will not only aid in future employment but take home for my own personal gains.</p>
+      <ul>
+        <li>Proven 11-year track record of working 12+ hours 7 days a week while maintaining a level of safety.</li>
+        <li>Using Design for Six Sigma tools, incorporated a software package into the Airbus A300 Primus EPIC integration program which has great potential of saving Honeywell at least $1mil across 20 other Primus programs.</li>
+        <li>Generated 50+ engineering change orders (ECO/ECN), which documented revisions made to 100+ drawings. Used internal validation process in accordance with design requirements and finalized customer approvals.</li>
+        <li>•	Managed and maintained multi-site project tracker for over 5000 parts with various manufacturing processes to include casting, machining, sheet metal, and additive manufacturing.</li>
+      </ul>
     </>
   )
 }
